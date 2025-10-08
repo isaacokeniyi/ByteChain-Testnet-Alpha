0e4b0e0aa18ff76a11a85b3e6f6f1d078f985221
Nifemi Bosun <144700801+nifemibosun@users.noreply.github.com>
2025-10-08 15:36:43 +0100

    Added new constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,18 +3,14 @@ const BLOCK_REWARD: number = 32;
 const MAX_NONCE_ATTEMPTS: number = 10_000_000;
 const BC_NAME: string = "0xByteChain";
 const BC_NAME_PUB: string = "0xByteChainPublicKey";
-const GEN_PREV_HASH: string = "0x00000000000000000000000000000000ByteChain";
-const GEN_CONTRACT_RECIPIENT: string = "0x000000000000000000000000000000000000000BC";
+const GEN_PREV_HASH: string = "0000000000000000000000000000000000ByteChain";
 
-const MIN_DIFFICULTY = 4;
-const MAX_DIFFICULTY = 10;
-const BLOCK_WINDOW = 4;
+const MIN_DIFFICULTY: number = 4;
+const MAX_DIFFICULTY: number = 10;
+const BLOCK_WINDOW_DIFF: number = 4; // block window for difficulty
+const BLOCK_WINDOW_FEE: number = 10;
 
-enum Tx_Type {
-    BYTE_TX = 'byte_tx',
-    CONTRACT = 'contract',
-    CONTRACT_CALL = 'contract_call'
-}
+const VANITY_ADDR: string = "00000000000000000000000000000000000000000";
 
 interface BlockHeader {
     nonce: number;
@@ -34,8 +30,8 @@ const print = (...data: any): void => {
 export { 
     BLOCK_TIME_DIFF, BLOCK_REWARD,
     BC_NAME, BC_NAME_PUB,
-    GEN_PREV_HASH, BLOCK_WINDOW, 
+    VANITY_ADDR, BLOCK_WINDOW_FEE,
+    GEN_PREV_HASH, BLOCK_WINDOW_DIFF, 
     MIN_DIFFICULTY, MAX_DIFFICULTY, 
-    MAX_NONCE_ATTEMPTS, GEN_CONTRACT_RECIPIENT,
-    BlockHeader, Tx_Type, print
+    MAX_NONCE_ATTEMPTS, BlockHeader, print
 };
